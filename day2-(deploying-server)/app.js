fd4e15bbae7f6259fbf71fcf631f719785dc98f5
bharatXjaidiya <jaidiyabharat16@gmail.com>
2026-03-25 02:00:16 +0530

    sever-deployed

--- day2-(deploying-server)/app.js
+++ day2-(deploying-server)/app.js
@@ -5,6 +5,10 @@ app.get('/',(req,res)=>{
     res.send("hey👋server is running and ready for deployement")
 })
 
+app.get('/about',(req,res)=>{
+    res.send("My self Bharat Jaidiya 😎")
+})
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
